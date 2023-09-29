8ead254dd2ae37475b3bdf627c50cc5feb3cca18
HP <HP@192.168.1.3>
2023-09-29 19:50:22 +0530

    first commit

--- corejava6/src/com/tnsif/override/Parent.java
+++ corejava6/src/com/tnsif/override/Parent.java
@@ -11,3 +11,29 @@ public class Parent {
 	}
 	
 }
+class Child extends Parent {
+    // new m1() method
+    // unique to Child class
+    private void m1()
+    {
+        System.out.println("From child m1()");
+    }
+ 
+    // overriding method
+    // with more accessibility
+    @Override public void m2()
+    {
+        System.out.println("From child m2()");
+    }
+}
+ 
+// Driver class
+class Main {
+    public static void main(String[] args)
+    {
+        Parent obj1 = new Parent();
+        obj1.m2();
+        Parent obj2 = new Child();
+        obj2.m2();
+    }
+}
